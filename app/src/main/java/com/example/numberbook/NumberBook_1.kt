227861b1409753a38ee88a194491ff326906a3f5
kimghem123 <82371258+kimghem123@users.noreply.github.com>
2022-04-06 01:01:34 +0900

    update

--- app/src/main/java/com/example/numberbook/NumberBook_1.kt
+++ app/src/main/java/com/example/numberbook/NumberBook_1.kt
@@ -3,17 +3,19 @@ package com.example.numberbook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
-import kotlin.math.log
 
 class NumberBook_1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_number_book1)
-        val numberList = ArrayList<numberBook>()
+
+        val numberBook = makeNumber()
+        creatNumberBook(numberBook)
+
+        /*val numberList = ArrayList<numberBook>()
         for(i in 0 until 20){
             numberList.add(numberBook("hello${i}","01000000000"))
         }
@@ -30,9 +32,54 @@ class NumberBook_1 : AppCompatActivity() {
                 intent.putExtra("phoneNum",numberList.get(i).number)
                 startActivity(intent)
             }
+        }*/
+
+    }
+
+    fun makeNumber(numberSize: Int = 10, person: Person = Person()): Person {
+        for (i in 0 until numberSize) {
+            person.addNumber(
+                numberBook("hello${i}", "01000000000")
+            )
+        }
+        return person
+    }
+
+    fun creatNumberBook(person: Person) {
+
+        val container = findViewById<LinearLayout>(R.id.number_book_container)
+        val inflater = layoutInflater
+
+        for (i in 0 until person.numberList.size) {
+            val numberView = inflater.inflate(R.layout.number_view, null)
+            val textView = numberView.findViewById<TextView>(R.id.number_btn)
+            textView.setText(person.numberList.get(i).name)
+            container.addView(numberView)
+            addClickListener(person.numberList.get(i),numberView)
+        }
+
+    }
+
+    fun addClickListener(number: numberBook, view: View){
+        view.setOnClickListener {
+            val intent = Intent(this, NumberBook_Inner::class.java)
+            intent.putExtra("name", number.name)
+            intent.putExtra("phoneNum", number.number)
+            startActivity(intent)
         }
     }
+
 }
-class numberBook(val name: String, val number: String){
+
+class Person() {
+    val numberList = ArrayList<numberBook>()
+
+    fun addNumber(number: numberBook) {
+        numberList.add(number)
+    }
+
+}
+
+class numberBook(val name: String, val number: String) {
 
 }
